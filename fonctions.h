2c019e2c87fe63be954e7c322527c4353b5f6ed6
borhenetlili <borhenetlili18@gmail.com>
2022-03-10 23:57:18 +0100

    Ajout header projet

---
+++ fonctions.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+void initPerso(Personne *p)
+void afficherPerso(Personne p, SDL_Surface * screen)
+void deplacerPerso (Personne *p, , int dt)
+void animerPerso (Personne* p)
+void saut (Personne* p)
+
+
+
